07555feb16d348990ea524527796ba345640076a
Mayank Rana <heymayanktapgro@gmail.com>
2023-12-22 11:06:36 +0530

    hii

--- src/app/PopupForm.jsx
+++ src/app/PopupForm.jsx
@@ -63,7 +63,7 @@ const PopupForm = ({ onClose }) => {
                 required
               />
               <label
-                htmlFor="name"
+               
                 className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
               >
                 Name
@@ -81,7 +81,7 @@ const PopupForm = ({ onClose }) => {
                 required
               />
               <label
-                htmlFor="tel"
+                
                 className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
               >
                 Phone number
@@ -99,7 +99,7 @@ const PopupForm = ({ onClose }) => {
                 required
               />
               <label
-                htmlFor="location"
+                
                 className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
               >
                 Location
