1f2a20ad918c931a936679207c0ec7f089de1726
Mayank Rana <heymayanktapgro@gmail.com>
2024-01-04 16:59:39 +0530

    API for google sheet

--- src/app/Contact.jsx
+++ src/app/Contact.jsx
@@ -1,29 +1,58 @@
 "use client";
 
+import axios from "axios";
+import { useState } from "react";
+
 export function Contact() {
+  const [name, setName]=useState('');
+  const [phone, setPhone]=useState('');
+  const [location, setLocation]=useState('');
+  const [company, setCompany]=useState('');
+
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const data = {
+      name: name,
+      phone: phone,
+      location:location,
+      company:company
+    };
+
+    axios.post('https://sheet.best/api/sheets/b304d009-864c-4663-a063-34c7978bc5e7', data)
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((error) => {
+        console.error('Error sending data:', error);
+      });
+  };
 
   return (
+ 
     <div className="place-items-center">
       <h2
         id="contact"
         className="font-bold text-3xl mt-5 tracking-tight text-center"
       >
-        Complete the form for free early access of TapGro.
+        Complete this Form to get a Free Early Access of TapGro.
               </h2>
       <div className="mt-15 container mx-auto grid h-full w-full grid-cols-1 lg:mt-7 lg:grid-cols-2">
         <div className="col-span-1">
-          <form className="max-w-lg rounded-lg shadow-xl p-3 mx-auto">
+          <form onSubmit={handleSubmit} className="max-w-lg rounded-lg shadow-xl p-3 mx-auto" name="submit-to-google-sheet">
             <div className="relative z-0 w-full mb-5 group">
-              <input
+            <input
                 type="text"
+                onChange={(e)=>setName(e.target.value)}
+                value={name}
                 name="name"
-                id="name"
+                id="phone"
                 className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
                 placeholder=""
                 required
               />
               <label
-                htmlFor="name"
+              htmlFor="name"
                 className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
               >
                 Name
@@ -32,6 +61,8 @@ export function Contact() {
             <div className="relative z-0 w-full mb-5 group">
               <input
                 type="tel"
+                onChange={(e)=>setPhone(e.target.value)}
+                value={phone}
                 name="phone"
                 id="phone"
                 className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
@@ -39,7 +70,7 @@ export function Contact() {
                 required
               />
               <label
-                htmlFor="tel"
+              htmlFor="phone"
                 className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
               >
                 Phone number
@@ -48,8 +79,10 @@ export function Contact() {
             <div className="relative z-0 w-full mb-5 group">
               <input
                 type="text"
-                name="loaction"
-                id="loaction"
+                name="location"
+                onChange={(e)=>setLocation(e.target.value)}
+                value={location}
+                id="location"
                 className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
                 placeholder=" "
                 required
@@ -64,6 +97,8 @@ export function Contact() {
             <div className="relative z-0 w-full mb-5 group">
               <select
                 id="company"
+                onChange={(e)=>setCompany(e.target.value)}
+                value={company}
                 name="company"
                 className="block text-gray-500 dark:text-gray-400 py-2.5 px-0 w-full text-sm bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 peer text-gray-500 dark:text-gray-400"
               >
@@ -85,7 +120,9 @@ export function Contact() {
             >
               Submit
             </button>
+            
           </form>
+          
         </div>
         <div>
           <iframe
